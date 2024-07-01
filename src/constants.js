1058e05603cdd7ce51194c4a82ee62e38648c24b
vito royeca <vito.royeca@outlook.com>
2024-07-01 12:37:48 -0400

    chang url endpoint

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const MAP_PK_TOKEN = "pk.eyJ1IjoibWlrZWthbGUiLCJhIjoiY2x4Z2JsdzZiMTIzbjJr
 export const IOS_CLIENT_ID = "308321760522-80c3t3bok4u6r45g1h2rfltkv4g8ebv9.apps.googleusercontent.com";
 export const ANDROID_CKIENT_ID = "308321760522-f00llrs1ktacks18u7q7rjodioequ3jn.apps.googleusercontent.com";
 export const WEB_CLIENT_ID = "308321760522-5qea4qreflrpiq6t68i3s2g1r1jffsll.apps.googleusercontent.com";
-// export const BACKEND_URL = "https://api.wildione.com";
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "https://api.wildione.com";
+// export const BACKEND_URL = "http://localhost:5000";
